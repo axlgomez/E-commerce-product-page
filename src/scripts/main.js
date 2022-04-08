a49bcb59055175d8a9ecfe46beb4c5160d289f2a
Axel Gomez <axelgomez.js@gmail.com>
2022-04-08 13:11:46 +0200

    everything related to styling

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -28,6 +28,10 @@ function openModal() {
   document.getElementById("myModal").style.display = "flex";
 }
 
+// Close the Modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
 
 let slideIndex = 1;
 showSlides(slideIndex);
